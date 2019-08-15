40c9433afe06092b8d4940360f70513f85adcbd9
SharmaCharu <53037307+SharmaCharu@users.noreply.github.com>
2019-08-16 00:07:04 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class main {
+	
+ public static void main(String[] args) {
+//	Vehicle vehicle1=new Vehicle();
+//	System.out.println(vehicle1.getName());
+//	vehicle1.setName("alto");
+//	System.out.println(vehicle1.getName());
+//	Vehicle vehicle2=new Vehicle("civic","black", "prime", "honda","200") ;
+//	System.out.println(vehicle2.getColor());
+//	vehicle2.setColor("green");
+//	System.out.println(vehicle2.getColor());
+//	System.out.println(vehicle2.getSpeed());
+//	
+   
+	Car car1=new Car("civic","black", "prime", "honda","200", false, true);
+//    System.out.println(car1.isLedScreen());
+     System.out.println(car1.getName());    
+}
+	
+
+}
